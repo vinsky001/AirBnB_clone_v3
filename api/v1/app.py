3f359911723a724de9cd5ecf622af9a45ba14bcd
papihoudini <ochiengcliff.co@gmail.com>
2023-07-30 14:25:54 -0400

    Updating....

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from  flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify
 
 
 # Create a Flask application instance
