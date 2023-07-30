76ef0de7d3761f8554b0b37323a5b718c02eeda6
papihoudini <ochiengcliff.co@gmail.com>
2023-07-30 06:14:50 -0400

    PEP8 style formatting

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 # Create a Flask application instance
 app = Flask(__name__)
-app.register_Blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
-def teardown_storage(error=none):
-    """ calls storage.close()"""
+def teardown_storage(error=None):
+    """calls storage.close()"""
     return storage.close()
 
+
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST') and port('HBNB_API_PORT')
-        app.run(host=getenv('HBNB_API_HOST'),
-            port=int(getenv('HBNB_API_PORT')), threaded=True
+    if getenv("HBNB_API_HOST") and port("HBNB_API_PORT"):
+        app.run(
+            host=getenv("HBNB_API_HOST"),
+            port=int(getenv("HBNB_API_PORT")),
+            threaded=True,
+        )
 
     else:
-        app.run(host='0.0.0.0', port=500, threaded=True)
+        app.run(host="0.0.0.0", port=5000, threaded=True)
