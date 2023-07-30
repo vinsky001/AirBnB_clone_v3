efaa2f70897d3a9040827fe2c0ae51810a70bd2c
papihoudini <ochiengcliff.co@gmail.com>
2023-07-30 06:01:54 -0400

    PEP8 style formatting

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,21 +20,24 @@ states_ = storage.count(State)
 users_ = storage.count(User)
 
 
-data = {"amenities": amenities_,
-        "cities": cities_,
-        "places": places_,
-        "reviews": reviews_,
-        "states": states_,
-        "users": users_
-        }
+data = {
+    "amenities": amenities_,
+    "cities": cities_,
+    "places": places_,
+    "reviews": reviews_,
+    "states": states_,
+    "users": users_,
+}
 
-@app_views.route('/status' strict_slashes=False)
+
+@app_views.route("/status", strict_slashes=False)
 def return_json():
     """returns status :ok"""
     status = {"status": "OK"}
     return jsonify(status)
 
-@app_views.route('/stats', strict_slashes=False)
+
+@app_views.route("/stats", strict_slashes=False)
 def obj_no():
     """retrieves the number of each objects by type"""
     return jsonify(data)
