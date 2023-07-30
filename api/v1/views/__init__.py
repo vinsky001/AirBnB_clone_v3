50a94c42e5513be1c285501c49e8c1a2fef1c90e
papihoudini <ochiengcliff.co@gmail.com>
2023-07-30 06:30:54 -0400

    Updating....

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,12 @@
 """this module creates app_views which is an instance of Blueprint
 """
 from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__)
+
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
-
-app_views = Blueprint("app_views", __name__)
