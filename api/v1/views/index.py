033300989222c9ca88070795c0bf88e9c85e7818
papihoudini <ochiengcliff.co@gmail.com>
2023-07-31 05:08:24 -0400

    Updating index.py file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
-"""this module has route /status
-"""
-from models import storage
-from models.state import State
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.user import User
-from flask import Flask, jsonify
+""" Index """
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
-amenities_ = storage.count(Amenity)
-cities_ = storage.count(City)
-places_ = storage.count(Place)
-reviews_ = storage.count(Review)
-states_ = storage.count(State)
-users_ = storage.count(User)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
-data = {
-    "amenities": amenities_,
-    "cities": cities_,
-    "places": places_,
-    "reviews": reviews_,
-    "states": states_,
-    "users": users_,
-}
-
-
-@app_views.route("/status", strict_slashes=False)
-def return_json():
-    """returns status :ok"""
-    status = {"status": "OK"}
-    return jsonify(status)
-
-
-@app_views.route("/stats", strict_slashes=False)
-def obj_no():
-    """retrieves the number of each objects by type"""
-    return jsonify(data)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
